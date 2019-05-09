53706ca366f1154cf4920fd132b5e80a5c43216a
devduttjangir <devdutt.jangir@3pillarglobal.com>
2019-05-09 14:46:54 +0530

    files removed

---
+++ precache-manifest.15382512c69433f2f2f07e254621904e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1ee71aa909b176c1a5f890c529c69a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a85d0ed372f46cf730b",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "ffae12e5dcb2cd603f82",
+    "url": "/static/js/2.ee0d59e6.chunk.js"
+  },
+  {
+    "revision": "7a85d0ed372f46cf730b",
+    "url": "/static/js/main.2e68afbc.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "529f7321c9821e24b57890841e443c5e",
+    "url": "/static/media/elec-waste.529f7321.jpg"
+  },
+  {
+    "revision": "13c6c99076353efe6827a93eef2f9b20",
+    "url": "/static/media/paper-waste.13c6c990.jpg"
+  },
+  {
+    "revision": "964b0be19dca94ef2aa63a61aa4e3b6d",
+    "url": "/static/media/plastic-waste.964b0be1.jpg"
+  },
+  {
+    "revision": "914d752ebe67abdbab97a7a6f4bd19a5",
+    "url": "/static/media/rubber-waste.914d752e.jpg"
+  }
+]);
